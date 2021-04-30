1ee685668a170ac3dea4f3ec6d79d1596658fadb
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-04-30 17:05:36 -0400

    working on contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,37 +25,72 @@ const defaultValue = {
   
 };
 
-function ContactForm() {
-  const [value, setValue] = useState(defaultValue);
+class Contact extends React.Component {
 
-  const [status, setStatus] = useState("Submit");
+  constructor(props) {
+    super(props);
+    this.state={
+      name: '',
+      email: '',
+      message: '',
+    }
+  }
+  // const [value, setValue] = useState(defaultValue);
 
-  const handleSubmit = async (e) => {
+  // const [status, setStatus] = useState("Submit");
+
+  onNameChange(event) {
+    this.setState({name: event.target.value})
+  }
+  onEmailChange(event) {
+    this.setState({email: event.target.value})
+  }
+  onMessageChange(event) {
+    this.setState({message: event.target.value})
+  }
+
+  submitEmail(e){
     e.preventDefault();
-    setStatus("Sending...");
-    const {name, email, message} = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("https://gintstir.github.io/react-portfolio/#/contact", {
+    axios({
       method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
+      url:"/send",
+      data: this.state
+    })
+    .then((response) => {
+      if(response.data.status === 'success') {
+        alert ()
+      }
+    })
+  }
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setStatus("Sending...");
+  //   const {name, email, message} = e.target.elements;
+  //   let details = {
+  //     name: name.value,
+  //     email: email.value,
+  //     message: message.value,
+  //   };
+  //   let response = await fetch("https://gintstir.github.io/react-portfolio/#/contact", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json;charset=utf-8",
+  //     },
+  //     body: JSON.stringify(details),
+  //   });
+  //   setStatus("Submit");
+  //   let result = await response.json();
+  //   alert(result.status);
+  // };
   return (
     <Grommet full  >
       <Box fill align="center" justify="center">
         <Text size="3xl" weight="bold" margin={{bottom: "medium"}}>Get in touch!</Text>
         <Box elevation="xlarge" width="large" pad="medium" background={{color:"white", opacity:"strong"}} border={{size:"medium", color:"black", style:"double"}}>
           <Form
+            id="contact-form"
+            onSubmit={this.submitEmail.bind(this)}
+            method="POST"            
             value={value}
             onChange={(nextValue, { touched }) => {
               console.log("Change", nextValue, touched);
@@ -65,12 +100,12 @@ function ContactForm() {
             // onSubmit={(event) =>
             //   console.log("Submit", event.value, event.touched)
             // }
-            onSubmit={handleSubmit}
+            // onSubmit={handleSubmit}
           >
-            <FormField htmlFor="name" label="Name" name="name" required>
+            <FormField htmlFor="name" label="Name" name="name" value={this.state.name} onChange={this.onNameChange.bind(this)} required>
               <TextInput name="name" />
             </FormField>
-            <FormField htmlFor="email" label="Email" name="email" required>
+            <FormField htmlFor="email" label="Email" name="email" value={this.state.email} onChange={this.onEmailChange.bind(this)} required >
               <MaskedInput
                 name="email"
                 mask={[
@@ -82,7 +117,7 @@ function ContactForm() {
                 ]}
               />
             </FormField>            
-            <FormField htmlFor="message" label="Message" name="message">
+            <FormField htmlFor="message" value={this.state.message} onChange={this.OnMessageChange.bind(this)} required label="Message" name="message">
               <TextArea name="message" />
             </FormField>
 
