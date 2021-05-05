b82561c7f2ee6d345967cf41fccb6c5114b74767
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-05-05 10:47:59 -0400

    finished contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,128 +4,67 @@ import React from "react";
 
 import {
   Box,
-  Carousel,
-  Button, 
-  Grid,
-  DropButton,
-  Layer,
-  Image, 
-  Form,
-  FormField,
-  Grommet,
-  MaskedInput,  
-//   Select,
-  Text,
-  TextArea,
-  TextInput,
+  Anchor,  
+  Image,   
+  Grommet,    
 } from "grommet";
 
-import {Close, Github, Linkedin, Send, Instagram} from 'grommet-icons'
 
-const data = [
-  {
-    buttonTitle: "Github",
-    buttonLink: "https://github.com/Gintstir",
-    qrCode: require('./qrcode_github.com.png').default,
-    buttonIcon: <Github />,
-  },
-  {
-    buttonTitle: "LInkedIn",
-    buttonLink: "https://www.linkedin.com/in/gintautas-stirbys/",
-    qrCode: require('./qrcode_www.linkedin.com.png').default,
-    buttonIcon: <Linkedin />,
-  },
-  {
-    buttonTitle: "Email",
-    buttonLink: "gint.stirbys@gmail.com",
-    qrCode: require('./generator.png').default,
-    buttonIcon: <Send />,
-  },
-  {
-    buttonTitle: "Instagram",
-    buttonLink: "https://www.instagram.com/gintstir/",
-    qrCode: require('./qrcode_www.instagram.com (1).png').default,
-    buttonIcon: <Instagram />,
-  }
-]
 
-// const DropContent = ({ onClose }) => {
+import {Github, Linkedin, Send, Instagram} from 'grommet-icons'
 
-//   {data.map((item) => (
-//     <Box pad="small" item={item}>
-//     <Box alignSelf="center"  height="small" width="small"><Image src={item.qrCode}></Image></Box>
-//   </Box>
-//   ))}
-// }
+//Leaving this data here so I can go back and DRY up the code below!
+// const data = [
+//   {
+//     buttonTitle: "Github",
+//     buttonLink: "https://github.com/Gintstir",
+//     qrCode: require('./generatorGithub.png').default,
+//     buttonIcon: <Github />,
+//   },
+//   {
+//     buttonTitle: "LInkedIn",
+//     buttonLink: "https://www.linkedin.com/in/gintautas-stirbys/",
+//     qrCode: require('./generatorLinkedin.png').default,
+//     buttonIcon: <Linkedin />,
+//   },
+//   {
+//     buttonTitle: "Email",
+//     buttonLink: "gint.stirbys@gmail.com",
+//     qrCode: require('./generator.png').default,
+//     buttonIcon: <Send />,
+//   },
+//   {
+//     buttonTitle: "Instagram",
+//     buttonLink: "https://www.instagram.com/gintstir/",
+//     qrCode: require('./generatorInsta.png').default,
+//     buttonIcon: <Instagram />,
+//   }
+// ]
+
 
 function ContactForm() {
-  // const [open, setOpen] = React.useState();
-  // // const [select, setSelect] = React.useState('');
-
-  // const onOpen = () => setOpen(true);
-  // const onClose = () => setOpen(false);
-
+  
   return (
-    <Grommet full  >
-      <Box fill overflow="hidden" pad="medium" justify="center" align="center" >
+    <Grommet  full  >
+      <Box fill overflow="auto" pad="small" justify="center" align="center" gap="small" direction="row" responsive wrap>
         
-        <Box height="medium" width="large"  background={{color:"light-3", opacity:"strong"}} round="large" >
-          <Carousel alignSelf="center" focusIndicator="false" controls="arrows">
-            <Box  width="medium" height="medium" align="center" justify="center" background={{color: "light-3", opacity:"strong"}} >
-              <Text><Linkedin/>LinkedIn</Text>
-              <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./qrcode_www.linkedin.com.png').default} /></Box>
+            <Box  round="small" margin="medium" width="small" height="small" align="center" justify="center" background={{color: "light-3", opacity:"strong"}} >
+              <Anchor textDecoration="none" className="contactLink" color="neutral-3"margin={{bottom: '20px'}} icon={<Linkedin/>} label="Linkedin" href="https://www.linkedin.com/in/gintautas-stirbys/" target="_blank" />
+              <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./generatorLinkedin.png').default} /></Box>
             </Box>
-            <Box  width="medium" height="medium" align="center" justify="center" background="light-3">
-              <Text><Github/>Github</Text>
-              <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./qrcode_github.com.png').default} /></Box>
+            <Box  round="small" margin="medium" width="small" height="small" align="center" justify="center" background={{color: "light-3", opacity:"strong"}}>
+              <Anchor className="contactLink" color="neutral-3" margin={{bottom: '20px'}} icon={<Github/>} label="Github" href="https://github.com/Gintstir" target="_blank" />
+              <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./generatorGithub.png').default} /></Box>
             </Box>
-            <Box  width="medium" height="medium" align="center" justify="center" background="light-3">
-              <Text><Send/>Email</Text>
+            <Box  round="small" margin="medium" width="small" height="small" align="center" justify="center" background={{color: "light-3", opacity:"strong"}} >
+              <Anchor className="contactLink" color="neutral-3" margin={{bottom: '20px'}} icon={<Send />} label="Email" href="mailto:gint.stirbys@gmail.com?subject=Hi Gint, just saw your portfolio!" target="_blank" />
               <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./generator.png').default} /></Box>
             </Box>
-            <Box  width="medium" height="medium" align="center" justify="center" background="light-3">
-              <Text> <Instagram/>Instagram</Text>
-              <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./qrcode_www.instagram.com (1).png').default} /></Box>
+            <Box  round="small" margin="medium" width="small" height="small" align="center" justify="center" background={{color: "light-3", opacity:"strong"}} >
+              <Anchor className="contactLink" color="neutral-3" margin={{bottom: '20px'}} icon={<Instagram/>} label="Instagram" href="https://www.instagram.com/gintstir/" taregt="_blank" />
+              <Box height="xsmall" width="xsmall"><Image  fit="contain" src={require('./generatorInsta.png').default} /></Box>
             </Box>
-          </Carousel>
-        </Box>
-
-        {/* <Grid gap="xsmall" columns={{count: "fit", size: "auto"}} rows={["xsmall", "xsmall", "xsmall", "xsmall"]} responsive alignSelf="center">
           
-            
-              {data.map ((item) => (
-                <Box pad="small" justify="center" align="center" item={item} key={item.buttonTitle}>
-                  <Box border width="300px" height="100px" align="center" justify="center" >
-                    <DropButton primary label={item.buttonTitle} fill color="neutral-3" icon={item.buttonIcon} onOpen={onOpen} onClose={onClose} dropContent={<DropContent onClose={onClose} /> } dropProps={{align: {left: "right"}}} />
-                    {open && (
-                      <Layer animation="fadeIn" background={{color:"light-3", opacity: "strong"}} full="vertical" position="right" modaL onClickOutside={onClose} onEsc={onClose}>
-                        <Box  overflow="auto" width="medium"  pad="medium" direction="row"><Button icon={<Close/>} onClick={onClose}></Button></Box>
-                        <Box alignSelf="center"  height="small" width="small"><Image src={item.qrCode}></Image></Box>
-                      </Layer>
-                    )}
-                  </Box>
-                </Box>
-              ))}
-                <Box  pad="small" justify="center" align="center" >
-                  <Box border width="300px" height="100px" align="center" justify="center" >
-                    <Button primary label="linkedin" fill color="neutral-3"/>
-                  </Box>
-                </Box>
-
-                <Box pad="small" justify="center" align="center" >
-                  <Box border width="300px" height="100px" align="center" justify="center" >
-                    <Button primary label="Email" fill color="neutral-3"/>
-                  </Box>
-                </Box>
-                
-                <Box pad="small" justify="center" align="center" >
-                  <Box border width="300px" height="100px" align="center" justify="center" >
-                    <Button primary label="Instagram" fill color="neutral-3"/>
-                  </Box>
-                </Box>
-               
-            
-        </Grid> */}
       </Box>
     </Grommet>
   );
