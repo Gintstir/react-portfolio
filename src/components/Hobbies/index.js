70eeced888d94135b2cee1312be8b1eb0cbee56d
Gint Stirbys <70384913+Gintstir@users.noreply.github.com>
2021-05-05 15:31:43 -0400

    adding boxes for future photo carousels

--- src/components/Hobbies/index.js
+++ src/components/Hobbies/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {Grommet, Box, Paragraph} from "grommet"
+import {Grommet, Box, Grid, Paragraph} from "grommet"
 //import background from '../../assets/images/forestbackdrop.jpg'
 
 
 function Hobbies() {
     return (
         <Grommet  full>
-            <Box fill align="center" justify="center">
-                <Box round="small" width="large" height="small" background={{color:"white", opacity:"strong"}} >
-                    <Paragraph textAlign="center" alignSelf="center">
-                        Coming soon!<br></br><br></br>
-                        This component is under construction!
-                    </Paragraph>
-                </Box>
+            <Box  fill gap="small" pad="medium" justify="center" wrap responsive>
+                <Grid  justify="center" gap="small" columns={{count:"fit", size: "small"}} alignContent="center" responsive={true}>
+                    <Box alignSelf="center" round="small" width="medium" height="small" background={{color:"white", opacity:"strong"}} >
+                    
+                    </Box>
+                    <Box alignSelf="center" round="small" width="medium" height="small" background={{color:"white", opacity:"strong"}} >
+                        
+                    </Box>
+                    <Box alignSelf="center" round="small" width="medium" height="small" background={{color:"white", opacity:"strong"}} >
+                        
+                    </Box>
+                    <Box alignSelf="center" round="small" width="medium" height="small" background={{color:"white", opacity:"strong"}} >
+                        
+                    </Box>
+                </Grid>
             </Box>
         </Grommet>
     )
